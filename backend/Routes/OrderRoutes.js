852f9411c1fa714e2bb0f02bfe3ef4942da05dbf
Coderdivine <chimdi4332@gmail.com>
2022-08-17 23:47:30 +0100

    be that person

---
+++ backend/Routes/OrderRoutes.js
@@ -0,0 +1,10 @@
+require("dotenv").config();
+const express = require("express");
+const router = express.Router();
+const uuid = require("uuid");
+const axios = require("axios");
+const request = require("request");
+const {product_detailss,order_detailss, payment_detailss} = require("../Model/index");
+//add a order,
+//update a order,
+//get order info,
