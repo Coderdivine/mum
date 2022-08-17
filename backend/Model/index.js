1b786c7dcf445387b13f990344aaeaeaae779915
Coderdivine <chimdi4332@gmail.com>
2022-08-17 22:25:37 +0100

    master

--- backend/Model/index.js
+++ backend/Model/index.js
@@ -1,9 +1,63 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const product_details = new Schema({})
+const product_details = new Schema({
+    name:String,
+    rate:String,
+    sold_out:Boolean,
+    quantity:String,
+    sold:String,
+    brand:{
+        type:String,
+        required:false
+    },
+    category:{
+        type:String,
+        required:true
+    },
+    id:String,
+    price:Number,
+    description:{
+        type:String,
+        required:true
+    },
+    sizes:String,
+    color:String,
+    image:String,
+    weight:String,
+    date:{
+        type:Date,
+        default:Date.now()
+    }
+})
 const product_detailss = mongoose.model("product_details",product_details);
 
-const order_details = new Schema({})
+const order_details = new Schema({
+    firstname:String,
+    lastname:String,
+    country:String,
+    address:String,
+    city:String,
+    phone_number:String,
+    email:String,
+    company:{
+        type:String,
+        required:false
+    },
+    product:[
+        {
+            name:String,
+            size:String,
+            color:String,
+            amount:String,
+            product_id:String,
+            quantity:String
+        }
+    ],
+    date:{
+        type:Date,
+        default:Date.now()
+    }
+})
 const order_detailss = mongoose.model("order_details",order_details);
 
 const payment_details = new Schema({})
