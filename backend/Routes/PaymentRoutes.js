72d7f6a593684738630350df7f271087a0aa9a02
Coderdivine <chimdi4332@gmail.com>
2022-08-18 00:19:47 +0100

    main

--- backend/Routes/PaymentRoutes.js
+++ backend/Routes/PaymentRoutes.js
@@ -5,3 +5,18 @@ const uuid = require("uuid");
 const axios = require("axios");
 const request = require("request");
 const {product_detailss,order_detailss, payment_detailss} = require("../Model/index");
+//send payment info 
+//then make payment
+//update status
+const contact_service = [{
+    number:"09161911394",
+    email:"chimdi4332@gmail.com",
+    address:"SHOP GA 11 Lagos island",
+}]
+router.post("/",async(req,res)=>{
+    try{
+
+    }catch(err){
+
+    }
+})
