ebe36566538c2f61c687a0c1af98fd6a031ccfa2
Coderdivine <chimdi4332@gmail.com>
2022-08-18 00:16:44 +0100

    main

---
+++ backend/Routes/PaymentRoutes.js
@@ -0,0 +1,7 @@
+require("dotenv").config();
+const express = require("express");
+const router = express.Router();
+const uuid = require("uuid");
+const axios = require("axios");
+const request = require("request");
+const {product_detailss,order_detailss, payment_detailss} = require("../Model/index");
