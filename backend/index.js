6bf0af9c0a3917a32fd9cf9248df32c58f1c768c
Coderdivine <chimdi4332@gmail.com>
2022-08-17 21:59:44 +0100

    master

--- backend/index.js
+++ backend/index.js
@@ -7,9 +7,9 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 const mongoose = require("mongoose");
 // "mongodb+srv://chimdi:chimdindu2@cluster0.5zspaed.mongodb.net/?retryWrites=true&w=majority" || 'mongodb://localhost:27017/users'
-mongoose.connect("mongodb+srv://chimdi:chimdindu2@cluster0.5zspaed.mongodb.net/?retryWrites=true&w=majority");
+mongoose.connect('mongodb://localhost:27017/mum');
 const port = process.env.PORT || 9099;
-const db = mongoose.connection
+const db = mongoose.connection;
 db.on("error",(err)=>{console.log(err)})
 db.once("open",()=> console.log("Connected to database"));
 app.get("/",(req,res)=>{
