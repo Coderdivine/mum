82339deb831786a8c4c970b8f4fb735551194c0e
Coderdivine <chimdi4332@gmail.com>
2022-08-17 22:24:06 +0100

    master

--- backend/Routes/ProductRoutes.js
+++ backend/Routes/ProductRoutes.js
@@ -4,4 +4,12 @@ const router = express.Router();
 const uuid = require("uuid");
 const axios = require("axios");
 const {product_detailss,order_detailss, payment_detailss} = require("../Model/index");
-//add
+//add a product,
+//edit a product,
+//delete a product,
+//get all product
+//get product info
+//
+router.post("/product-details",(req,res)=>{
+    
+})
