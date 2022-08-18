19e64c44c36b6ff88106943be7f66ca3f7d65649
Coderdivine <chimdi4332@gmail.com>
2022-08-18 12:11:53 +0100

    message

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,12 @@ const app = express();
 app.use(cors());
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
+const OrderRoutes = require("./Routes/OrderRoutes");
+app.use("/order",OrderRoutes);
+const PaymentRoutes = require("./Routes/PaymentRoutes");
+app.use("/payment",PaymentRoutes);
+const ProductRoutes = require("./Routes/ProductRoutes");
+app.use("/product",ProductRoutes);
 const mongoose = require("mongoose");
 // "mongodb+srv://chimdi:chimdindu2@cluster0.5zspaed.mongodb.net/?retryWrites=true&w=majority" || 'mongodb://localhost:27017/users'
 mongoose.connect('mongodb://localhost:27017/mum');
@@ -12,6 +18,7 @@ const port = process.env.PORT || 9099;
 const db = mongoose.connection;
 db.on("error",(err)=>{console.log(err)});
 db.once("open",()=> console.log("Connected to database"));
+
 app.get("/",(req,res)=>{
     res.json({
         message:"Welcome to Mum's shop",
