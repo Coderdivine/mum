d78e96c88c01782e82dae4e0dad818ac94e63c0b
Coderdivine <chimdi4332@gmail.com>
2022-08-22 11:10:09 +0100

    be that person

--- frontend/E-CommerceWebsite/payment.js
+++ frontend/E-CommerceWebsite/payment.js
@@ -1,6 +1,6 @@
 console.clear();
-let account_name = document.getElementById("");
-let account_name = document.getElementById("");
-let account_name = document.getElementById("");
-let account_name = document.getElementById("");
-let account_name;
+let account_name = document.getElementById("acc_name");
+let account_number = document.getElementById("acc_number");
+let bank_name = document.getElementById("bank_name");
+let amount;
+let order_id;
