859a5c50e32c4dc78db8c65ce310ef9fc4f679a0
Coderdivine <chimdi4332@gmail.com>
2022-08-22 12:37:15 +0100

    222

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ const ProductRoutes = require("./Routes/ProductRoutes");
 
 const mongoose = require("mongoose");
 //"mongodb+srv://chimdi:chimdindu2@cluster0.5zspaed.mongodb.net/?retryWrites=true&w=majority" || 'mongodb://localhost:27017/mum'
-mongoose.connect('mongodb://localhost:27017/mum');
+mongoose.connect("mongodb+srv://chimdi:chimdindu2@cluster0.5zspaed.mongodb.net/?retryWrites=true&w=majority");
 const port = process.env.PORT || 9099;
 const db = mongoose.connection;
 db.on("error",(err)=>{console.log(err)});
