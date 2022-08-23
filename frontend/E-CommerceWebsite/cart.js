d2ad06be433a050f65bd774e8ad78d9c0f9218d5
Coderdivine <chimdi4332@gmail.com>
2022-08-23 13:56:28 +0100

    main

--- frontend/E-CommerceWebsite/cart.js
+++ frontend/E-CommerceWebsite/cart.js
@@ -250,7 +250,7 @@ httpRequest.onreadystatechange = function()
             }
             shipFee(product,totalAmount)
             amountUpdate(totalAmount)
-            let ff_ = shipFee(product,totalAmount) + totalAmount;
+            let ff_ = totalAmount + (product.length * 1500);
             sessionStorage.setItem("amount",ff_);
             sessionStorage.setItem("amount",ff_);
             console.log(ff_);
