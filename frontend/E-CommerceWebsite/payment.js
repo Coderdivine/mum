c49de5ffd1562f0ae1c346d66a73bf5476e25ce9
Coderdivine <chimdi4332@gmail.com>
2022-08-23 23:03:01 +0100

    main

--- frontend/E-CommerceWebsite/payment.js
+++ frontend/E-CommerceWebsite/payment.js
@@ -44,6 +44,7 @@ btn.addEventListener("click",function(){
             btn_two.style = "display:grid;";
 
             main_change.innerHTML = "";
+            const ans = res.data.data;
             //please pay ${amount} to this account
             //after 15:00 this order will expire.
             ` <div id="main_change" class="main_change">
