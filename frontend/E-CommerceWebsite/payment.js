e730d2cbf24ad74d40a8afe3d82bd31ee88f980c
Coderdivine <chimdi4332@gmail.com>
2022-08-23 22:35:30 +0100

    main

--- frontend/E-CommerceWebsite/payment.js
+++ frontend/E-CommerceWebsite/payment.js
@@ -39,7 +39,8 @@ btn.addEventListener("click",function(){
             console.log(res.data);
             sessionStorage.setItem("order",true);
             btn.disabled = true;
-            btn_two.style = "display:grid;"
+            btn_two.style = "display:none;";
+            btn_two.style = "display:grid;";
                 }).catch(err=>{
                     alert("Please try again something went wrong");
                     console.log(err)
