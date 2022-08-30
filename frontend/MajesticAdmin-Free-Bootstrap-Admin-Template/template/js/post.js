b1b57797ea7abc0fdabfda490775238dfec3e46f
Coderdivine <chimdi4332@gmail.com>
2022-08-30 22:00:08 +0100

    main

--- frontend/MajesticAdmin-Free-Bootstrap-Admin-Template/template/js/post.js
+++ frontend/MajesticAdmin-Free-Bootstrap-Admin-Template/template/js/post.js
@@ -13,7 +13,25 @@ let img_btn = document.getElementById("img_btn");
 let img_json = [];
 img_btn.addEventListener("click",function(){
     if(image.files[0]){
-
+        const form = new FormData();
+        form.append("file",img);
+        form.append("upload_preset","jawkxoys");
+        axios({
+            method:"POST",
+            url:"https://api.cloudinary.com/v1_1/axgura/image/upload",
+            data:form,
+            header:{"Content-Type":"multipart/form-data"},
+          })
+          .then((response)=>{
+            setImg(response.data.secure_url)
+            console.log(response.data.secure_url)
+            setCont(true);
+            setMsg("Uploaded");
+            getQueue()
+          }).catch((err)=>{
+            setMsg("Please select an image");
+            playSound("cancel")
+          })
     }else{
         alert("Please choose a image first")
     }
