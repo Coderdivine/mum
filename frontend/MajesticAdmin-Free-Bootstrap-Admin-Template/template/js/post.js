b9257f6aa252a1cf1289273f6ffa3ab7959f84c7
Coderdivine <chimdi4332@gmail.com>
2022-08-30 17:12:21 +0100

    main

---
+++ frontend/MajesticAdmin-Free-Bootstrap-Admin-Template/template/js/post.js
@@ -0,0 +1,7 @@
+let name = document.getElementById("name");
+let description = document.getElementById("description");
+let brand = document.getElementById("brand");
+let category = document.getElementById("category");
+let price = document.getElementById("price");
+let rate = document.getElementById("rate");
+let name = document.getElementById("name");
