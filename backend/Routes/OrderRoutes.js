b2cdeb93dcfe4b247159348179cd388ed2e42791
Coderdivine <chimdi4332@gmail.com>
2022-08-30 11:49:43 +0100

    main

--- backend/Routes/OrderRoutes.js
+++ backend/Routes/OrderRoutes.js
@@ -220,6 +220,9 @@ async function IncrementProduct(data){
 
 };
 async function sendMail({email,message,data},res){
+    const products = data.product.map(x=>`
+    <div>
+    </div>`)
     try{
         const options = {
             method: 'POST',
