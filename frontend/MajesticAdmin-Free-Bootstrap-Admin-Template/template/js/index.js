6920c236c839bd7cdfc2a9a77a23b39b476422d9
Coderdivine <chimdi4332@gmail.com>
2022-08-31 22:06:26 +0100

    main

--- frontend/MajesticAdmin-Free-Bootstrap-Admin-Template/template/js/index.js
+++ frontend/MajesticAdmin-Free-Bootstrap-Admin-Template/template/js/index.js
@@ -2,7 +2,7 @@ let order = document.getElementById("order");
 let total_sales = document.getElementById("total-sales");
 let rev = document.getElementById("rev");
 let total_views = document.getElementById("total_views");
-
+let urls = "https://ax-mum.herokuapp.com";
 
 
 async function GetData(){
