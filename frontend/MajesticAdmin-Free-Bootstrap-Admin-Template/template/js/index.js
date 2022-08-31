9e285c49a01814cb90a881048573d95a8770ee4f
Coderdivine <chimdi4332@gmail.com>
2022-08-31 22:34:42 +0100

    main

--- frontend/MajesticAdmin-Free-Bootstrap-Admin-Template/template/js/index.js
+++ frontend/MajesticAdmin-Free-Bootstrap-Admin-Template/template/js/index.js
@@ -1,5 +1,3 @@
-const { default: axios } = require("axios");
-
 let order = document.getElementById("order");
 let total_sales = document.getElementById("total-sales");
 let rev = document.getElementById("rev");
