319c4c973d7be53421c784c694fb33eccdb59190
Coderdivine <chimdi4332@gmail.com>
2022-08-31 22:04:47 +0100

    main

--- frontend/MajesticAdmin-Free-Bootstrap-Admin-Template/template/js/index.js
+++ frontend/MajesticAdmin-Free-Bootstrap-Admin-Template/template/js/index.js
@@ -2,3 +2,10 @@ let order = document.getElementById("order");
 let total_sales = document.getElementById("total-sales");
 let rev = document.getElementById("rev");
 let total_views = document.getElementById("total_views");
+
+
+
+async function GetData(){
+
+}
+GetData()
