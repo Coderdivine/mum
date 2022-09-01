592bd69bcd62fb745ca5cd28a7e084243f6b29e1
Coderdivine <chimdi4332@gmail.com>
2022-09-02 00:16:26 +0100

    man

--- frontend/E-CommerceWebsite/content.js
+++ frontend/E-CommerceWebsite/content.js
@@ -15,8 +15,7 @@ function dynamicClothingSection(ob) {
   let imgTag = document.createElement("img");
   imgTag.id = 'image1'
   imgTag.id = ob.name
-  if(typeof ob.image == "object"){
-    console.log("is")
+  if(typeof ob.image == "string"){
     let use = JSON.parse(ob.image);
     console.log("use",use[0]);
     console.log(use[0].substring(4,(use[0].length-4)))
