9a91363949e6e75dbc290f52f990a6342efa5509
Coderdivine <chimdi4332@gmail.com>
2022-09-01 11:41:39 +0100

    main

--- frontend/MajesticAdmin-Free-Bootstrap-Admin-Template/template/js/index.js
+++ frontend/MajesticAdmin-Free-Bootstrap-Admin-Template/template/js/index.js
@@ -4,41 +4,43 @@ let rev = document.getElementById("rev");
 let total_views = document.getElementById("total_views");
 let urls = "https://ax-mum.herokuapp.com";
 
-async function SumUp(data){
+async function SumUp(data) {
     let sum = 0;
     const val = await data && data.forEach(list => {
-    sum+=Number(list.price);
+        sum += Number(list.price);
     });
-total_views.innerHTML = val;
+    total_views.innerHTML = val;
 }
-async function GetData(){
+async function GetData() {
     urls = `${urls}/product/all-product`;
-   axios.get(urls).then(async(res)=>{
-    let data = res.data.data;
-     await SumUp(data);
-     urls = `https://ax-mum.herokuapp.com/order/all-order-info`
-     const show_ = await axios.get(urls);
-     if(show_.data){
-        data = show_.data.data;
-        let sum = 0;
-    const val =  await data && data.forEach(list => {
-          sum+=Number(list.price);
-     })
-     total_sales.innerHTML = val;
-     }else{
-        alert("Show was not found");
-     }
-   }).catch(err=>{
-    alert(`Error: ${err.message}`)
-   })
+    axios.get(urls).then(async (res) => {
+        let data = res.data.data;
+        await SumUp(data);
+        urls = `https://ax-mum.herokuapp.com/order/all-order-info`
+        const show_ = await axios.get(urls);
+        if (show_.data) {
+            data = show_.data.data;
+            let sum = 0;
+            if (data[0].price) {
+                const val = await data && data.forEach(list => {
+                    sum += Number(list.price);
+                })
+                total_sales.innerHTML = val;
+            }
+        } else {
+            alert("Show was not found");
+        }
+    }).catch(err => {
+        alert(`Error: ${err.message}`)
+    })
 }
 GetData()
-async function GetOrderData(){
+async function GetOrderData() {
     urls = "https://ax-mum.herokuapp.com";
     urls = `${urls}/order/all-order-info`;
-    axios.get(urls).then(res=>{
-     let data = res.data.data;
-     let mapped = data.map(x=>`
+    axios.get(urls).then(res => {
+        let data = res.data.data;
+        let mapped = data.map(x => `
      <tr>
      <td><a href="pages/product.html">${x.firstname} ${x.lastname}</a></td>
                             <td>${x.type}</td>
@@ -47,9 +49,9 @@ async function GetOrderData(){
                             <td>${x.phone_number}</td>
                             <td>${x.email}</td></tr>
      `);
-     order.innerHTML = mapped;
-    }).catch(err=>{
-     alert(`Error: ${err.message}`)
+        order.innerHTML = mapped;
+    }).catch(err => {
+        alert(`Error: ${err.message}`)
     })
 }
 GetOrderData()
