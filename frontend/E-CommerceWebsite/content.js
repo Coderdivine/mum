1d5937d5493e376e4e22a253ecff6ea6a60acb94
Coderdivine <chimdi4332@gmail.com>
2022-09-02 00:08:12 +0100

    main

--- frontend/E-CommerceWebsite/content.js
+++ frontend/E-CommerceWebsite/content.js
@@ -17,8 +17,8 @@ function dynamicClothingSection(ob) {
   imgTag.id = ob.name
   if(typeof ob.image !== "string"){
     let use = JSON.parse(ob.image);
-  console.log("use",use);
-    imgTag.src = use[0];
+    console.log("use",use);
+    //imgTag.src = use[0];
     console.log("use => 0",use[0])
   }else{
     // let use = JSON.parse(ob.image);
